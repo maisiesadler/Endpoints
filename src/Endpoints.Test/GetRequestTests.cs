e89baa6003f0ad8212de4c7e04ae910d8708b659
Maisie <maisiesadler@hotmail.com>
2020-12-24 07:43:17 +0000

    implement GetRequestTests

--- src/Endpoints.Test/GetRequestTests.cs
+++ src/Endpoints.Test/GetRequestTests.cs
@@ -1,181 +1,180 @@
-// using System.Threading.Tasks;
-// using System.Net;
-// using Xunit;
-// using Microsoft.Extensions.DependencyInjection;
-// using Endpoints.Api.Pipelines;
-// using Microsoft.AspNetCore.Builder;
-// using Endpoints.Instructions;
-// using Endpoints.Pipelines;
-// using Microsoft.AspNetCore.Http;
-// using System.Threading;
-// using Endpoints.Extensions;
+using System.Threading.Tasks;
+using System.Net;
+using Xunit;
+using Microsoft.Extensions.DependencyInjection;
+using Endpoints.Api.Pipelines;
+using Microsoft.AspNetCore.Builder;
+using Endpoints.Pipelines;
+using Microsoft.AspNetCore.Http;
+using Endpoints.Extensions;
 
-// namespace Endpoints.Test
-// {
-//     public class GetRequestTests : IClassFixture<TestFixture>
-//     {
-//         private readonly TestFixture _fixture;
+namespace Endpoints.Test
+{
+    public class GetRequestTests : IClassFixture<TestFixture>
+    {
+        private readonly TestFixture _fixture;
 
-//         public GetRequestTests(TestFixture fixture)
-//         {
-//             _fixture = fixture;
-//         }
+        public GetRequestTests(TestFixture fixture)
+        {
+            _fixture = fixture;
+        }
 
-//         public class TestGetPipeline : Pipeline<ModelRequest, ModelResponse>
-//         {
-//             private readonly IDbThing _dbThing;
+        public class DatabaseRetriever : IRetriever<ModelRequest, ModelResponse>
+        {
+            private readonly IDbThing _dbThing;
 
-//             public TestGetPipeline(IDbThing dbThing)
-//             {
-//                 _dbThing = dbThing;
-//             }
+            public DatabaseRetriever(IDbThing dbThing)
+            {
+                _dbThing = dbThing;
+            }
 
-//             protected override async Task<ModelResponse> GetResponse(ModelRequest input)
-//             {
-//                 return await _dbThing.GetModel(input);
-//             }
+            public async Task<ModelResponse> Retrieve(ModelRequest input)
+            {
+                return await _dbThing.GetModel(input);
+            }
+        }
 
-//             protected override ModelRequest ParseModel(HttpContext context)
-//             {
-//                 return new ModelRequest
-//                 {
-//                     Id = context.Request.RouteValues["id"]?.ToString(),
-//                 };
-//             }
+        public static class ModelParser
+        {
+            public static ModelRequest ParseModel(HttpContext context)
+            {
+                return new ModelRequest
+                {
+                    Id = context.Request.RouteValues["id"]?.ToString(),
+                };
+            }
 
-//             protected override async Task ParseResponse(HttpContext context, ModelResponse response)
-//             {
-//                 context.Response.StatusCode = (int)HttpStatusCode.OK;
-//                 await context.Response.WriteAsync(response.Name);
-//             }
-//         }
+            public static async Task ParseResponse(HttpContext context, ModelResponse response)
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.OK;
+                await context.Response.WriteAsync(response.Name);
+            }
+        }
 
-//         [Fact]
-//         public async Task GetWithNoParameters()
-//         {
-//             // Arrange
-//             using var server = _fixture.CreateServer(services =>
-//             {
-//                 services.AddSingleton<IDbThing, DbThing>();
-//                 services.AddTransient<Pipeline<ModelRequest, ModelResponse>, TestGetPipeline>();
-//                     // new PipelineInstructions<ModelRequest, ModelResponse>(stages => new MyModelPipeline(stages))
-//                     //     .Register<GetModelFromDatabase>()
-//                     //     .GetPipeline(sp));
-//             },
-//             app => app.UseEndpoints(endpoints =>
-//             {
-//                 endpoints.MapGet("/test", async ctx => await endpoints.ServiceProvider.GetRequiredService<Pipeline<ModelRequest, ModelResponse>>().Run(ctx));
-//             }));
-//             var client = server.CreateClient();
+        [Fact]
+        public async Task GetWithNoParameters()
+        {
+            // Arrange
+            using var server = _fixture.CreateServer(services =>
+            {
+                services.AddSingleton<IDbThing, DbThing>();
+                services.AddTransient<DatabaseRetriever>();
+                services.AddPipelines();
+                services.RegisterRetrievePipeline<ModelRequest, ModelResponse>(
+                    ModelParser.ParseModel,
+                    ModelParser.ParseResponse
+                );
+            },
+            app => app.UseEndpoints(endpoints =>
+            {
+                var registry = endpoints.ServiceProvider.GetRequiredService<PipelineRegistry>();
+                endpoints.MapGet("/test", registry.GetRetrieve<DatabaseRetriever, ModelRequest, ModelResponse>());
+            }));
+            var client = server.CreateClient();
 
-//             // Act
-//             var response = await client.GetAsync("/test");
+            // Act
+            var response = await client.GetAsync("/test");
 
-//             // Assert
-//             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
 
-//             var content = await response.Content.ReadAsStringAsync();
-//             Assert.Equal("0=", content);
-//         }
+            var content = await response.Content.ReadAsStringAsync();
+            Assert.Equal("0=", content);
+        }
 
-//         [Theory]
-//         [InlineData("test1")]
-//         [InlineData("anothername")]
-//         public async Task GetWithStringParameter(string @param)
-//         {
-//             // Arrange
-//             using var server = _fixture.CreateServer(services =>
-//             {
-//                 services.AddSingleton<IDbThing, DbThing>();
-//                 services.AddTransient<Pipeline<ModelRequest, ModelResponse>>(sp =>
-//                     new PipelineInstructions<MyModelPipeline, ModelRequest, ModelResponse>()
-//                         .WithStage<GetModelFromDatabase>()
-//                         .GetPipeline(sp));
-//             },
-//             app => app.UseEndpoints(endpoints =>
-//             {
-//                 endpoints.MapGet("/test/{id}", async ctx => await endpoints.ServiceProvider.GetRequiredService<Pipeline<ModelRequest, ModelResponse>>().Run(ctx));
-//             }));
-//             var client = server.CreateClient();
+        [Theory]
+        [InlineData("test1")]
+        [InlineData("anothername")]
+        public async Task GetWithStringParameter(string @param)
+        {
+            // Arrange
+            using var server = _fixture.CreateServer(services =>
+            {
+                services.AddSingleton<IDbThing, DbThing>();
+                services.AddTransient<DatabaseRetriever>();
+                services.AddPipelines();
+                services.RegisterRetrievePipeline<ModelRequest, ModelResponse>(
+                    ModelParser.ParseModel,
+                    ModelParser.ParseResponse
+                );
+            },
+            app => app.UseEndpoints(endpoints =>
+            {
+                var registry = endpoints.ServiceProvider.GetRequiredService<PipelineRegistry>();
+                endpoints.MapGet("/test/{id}", registry.GetRetrieve<DatabaseRetriever, ModelRequest, ModelResponse>());
+            }));
+            var client = server.CreateClient();
 
-//             // Act
-//             var response = await client.GetAsync("/test/" + @param);
+            // Act
+            var response = await client.GetAsync("/test/" + @param);
 
-//             // Assert
-//             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
 
-//             var content = await response.Content.ReadAsStringAsync();
-//             Assert.Equal("0=" + @param, content);
-//         }
+            var content = await response.Content.ReadAsStringAsync();
+            Assert.Equal("0=" + @param, content);
+        }
 
-//         public class TwoIdsModelRequest
-//         {
-//             public string Id { get; set; }
-//             public string Id2 { get; set; }
-//         }
+        public class TwoIdsModelRequest
+        {
+            public string Id { get; set; }
+            public string Id2 { get; set; }
+        }
 
-//         public class TwoIdsPipeline : StagedPipeline<TwoIdsModelRequest, ModelResponse>
-//         {
-//             public TwoIdsPipeline(PipelineStage<TwoIdsModelRequest, ModelResponse> stages)
-//                 : base(stages)
-//             {
-//             }
+        public class TwoIdsModelParser
+        {
+            public static TwoIdsModelRequest ParseModel(HttpContext context)
+            {
+                return new TwoIdsModelRequest
+                {
+                    Id = context.Request.RouteValues["id"]?.ToString(),
+                    Id2 = context.Request.RouteValues["id2"]?.ToString(),
+                };
+            }
+        }
 
-//             protected override TwoIdsModelRequest ParseModel(HttpContext context)
-//             {
-//                 return new TwoIdsModelRequest
-//                 {
-//                     Id = context.Request.RouteValues["id"]?.ToString(),
-//                     Id2 = context.Request.RouteValues["id2"]?.ToString(),
-//                 };
-//             }
+        public class ModelFromTwoIdsRetriever : IRetriever<TwoIdsModelRequest, ModelResponse>
+        {
+            public Task<ModelResponse> Retrieve(TwoIdsModelRequest input)
+            {
+                var response = new ModelResponse
+                {
+                    Name = $"Id = {input.Id}, Id2 = {input.Id2}",
+                };
 
-//             protected override async Task ParseResponse(HttpContext context, ModelResponse response)
-//             {
-//                 context.Response.StatusCode = (int)HttpStatusCode.OK;
-//                 await context.Response.WriteAsync(response.Name);
-//             }
-//         }
+                return Task.FromResult(response);
+            }
+        }
 
-//         public class TestPipelineStage : PipelineStage<TwoIdsModelRequest, ModelResponse>
-//         {
-//             public override Task<ModelResponse> RunAsync(TwoIdsModelRequest input, CancellationToken stoppingToken)
-//             {
-//                 var response = new ModelResponse
-//                 {
-//                     Name = $"Id = {input.Id}, Id2 = {input.Id2}",
-//                 };
+        [Fact]
+        public async Task GetWithMultipleStringParameters()
+        {
+            // Arrange
+            using var server = _fixture.CreateServer(services =>
+            {
+                services.AddSingleton<IDbThing, DbThing>();
+                services.AddTransient<ModelFromTwoIdsRetriever>();
+                services.AddPipelines();
+                services.RegisterRetrievePipeline<TwoIdsModelRequest, ModelResponse>(
+                    TwoIdsModelParser.ParseModel,
+                    ModelParser.ParseResponse
+                );
+            },
+            app => app.UseEndpoints(endpoints =>
+            {
+                var pipelineRegistry = endpoints.ServiceProvider.GetRequiredService<PipelineRegistry>();
+                endpoints.MapGet("/test/{id}/{id2}", pipelineRegistry.GetRetrieve<ModelFromTwoIdsRetriever, TwoIdsModelRequest, ModelResponse>());
+            }));
+            var client = server.CreateClient();
 
-//                 return Task.FromResult(response);
-//             }
-//         }
+            // Act
+            var response = await client.GetAsync("/test/one/two");
 
-//         [Fact]
-//         public async Task GetWithMultipleStringParameters()
-//         {
-//             // Arrange
-//             using var server = _fixture.CreateServer(services =>
-//             {
-//                 services.AddSingleton<IDbThing, DbThing>();
-//                 services.AddPipelines()
-//                     .RegisterPipeline<TwoIdsPipeline, TwoIdsModelRequest, ModelResponse>(
-//                         b => b.WithStage<TestPipelineStage>());
-//             },
-//             app => app.UseEndpoints(endpoints =>
-//             {
-//                 var pipelineRegistry = endpoints.ServiceProvider.GetRequiredService<PipelineRegistry>();
-//                 endpoints.MapGet("/test/{id}/{id2}", pipelineRegistry.Get<TwoIdsPipeline, TwoIdsModelRequest, ModelResponse>());
-//             }));
-//             var client = server.CreateClient();
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
 
-//             // Act
-//             var response = await client.GetAsync("/test/one/two");
-
-//             // Assert
-//             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-
-//             var content = await response.Content.ReadAsStringAsync();
-//             Assert.Equal("Id = one, Id2 = two", content);
-//         }
-//     }
-// }
+            var content = await response.Content.ReadAsStringAsync();
+            Assert.Equal("Id = one, Id2 = two", content);
+        }
+    }
+}
